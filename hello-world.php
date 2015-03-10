4b00a9602e3bc8b9fe334bb8373ae3d54b049406
sidewinder666 <mhiggins@usinternet.com>
2015-03-09 19:18:02 -0500

    oops
    \ and not /

--- hello-world.php
+++ hello-world.php
@@ -2,5 +2,5 @@
 echo 'hello world'
 echo 'This is my very first PHP program'
 echo 'and it actually works. Amazing, innit?'
-echo 'And I/'ve added one more line to see what that does to github app.'
+echo 'And I\'ve added one more line to see what that does to github app.'
 ?>
