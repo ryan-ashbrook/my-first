98fa301574ea0088612181f112f9146d35bb2473
sidewinder666 <mhiggins@usinternet.com>
2015-03-09 19:19:00 -0500

    Fucking Oops again
    Plus those pesky ; symbol

--- hello-world.php
+++ hello-world.php
@@ -1,6 +1,6 @@
 <?php
-echo 'hello world'
-echo 'This is my very first PHP program'
-echo 'and it actually works. Amazing, innit?'
-echo 'And I\'ve added one more line to see what that does to github app.'
+echo 'hello world';
+echo 'This is my very first PHP program';
+echo 'and it actually works. Amazing, innit?';
+echo 'And I\'ve added one more line to see what that does to github app.';
 ?>
